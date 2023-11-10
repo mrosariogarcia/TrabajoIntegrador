8b6ac00a5ee087c0927ebbfef088ab1c655d80bf
feli-qm <fquirogamange@udesa.edu.ar>
2023-11-10 15:24:45 -0300

    Results endpoint

--- js/searchResults.js
+++ js/searchResults.js
@@ -4,7 +4,9 @@ let resultado = location.search;
 let resultadoObj = new URLSearchParams(resultado);
 let buscado = resultadoObj.get("nombre");
 
-fetch(`https://api.themoviedb.org/3/search/movie?api_key=${5990392eb4d2b299d1f882dd6addfeae}&query=${buscado}`)
+let API_KEY = "5990392eb4d2b299d1f882dd6addfeae"
+
+fetch(`https://api.themoviedb.org/3/search/movie?query=${buscado}&api_key=${API_KEY}`)
 .then(function(response){
     return response.json()
 })
