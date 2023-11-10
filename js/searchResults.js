6d4c3be02f6b4565b8e818e5d58e66483fada99f
tfontanbalestra <tfontanbalestra@udesa.edu.ar>
2023-11-10 18:25:22 -0300

    avanzamos con la busqueda

--- js/searchResults.js
+++ js/searchResults.js
@@ -4,9 +4,7 @@ let resultado = location.search;
 let resultadoObj = new URLSearchParams(resultado);
 let buscado = resultadoObj.get("nombre");
 
-let API_KEY = "5990392eb4d2b299d1f882dd6addfeae"
-
-let id_movie = 16418
+const API_KEY = "5990392eb4d2b299d1f882dd6addfeae"
 
 fetch(`https://api.themoviedb.org/3/search/movie?query=${buscado}&api_key=${API_KEY}`)
 .then(function(response){
@@ -16,11 +14,39 @@ fetch(`https://api.themoviedb.org/3/search/movie?query=${buscado}&api_key=${API_
 .then(function(data){
     
     let info = data.results
-    for (let i = 0; i<=info.lenght; i++){
-        let imagen  = info[i].poster_path
-        let foto = 'https://image.tmdb.org/t/p/w500/${imagen}'
+
+    for (let i=0; i<=info.length; i++){
+        
+        //imagen de pelicula
+        let foto  = info[i].poster_path
+        let imagen = `https://image.tmdb.org/t/p/w342/${foto}`
+        
+        //titulo de pelicula
+        let titulo = info[i].title
+        
+        //id de pelicula
+        let id = info[i].id
+
+        //release date de pelicula
+        let releaseDate = info[1].release_date
+
+        //tendria que ir un if por si la "linea" no tiene imagen
+
+        //creamos una variable 'devolucion' para guardar todas las opciones de peliculas y luego, poder mostrarlas en el html
+        let devolucion = ""
+
+        devolucion += `<article>
+                        <img src="${imagen}">
+                        <a href="detailSerie.html?id=${id}">
+                            <h5>${titulo}</h5>
+                        </a>
+                        <h6>${releaseDate}</h6>
+                    </article>`
+
     }
-    console.log(info)
+    
+    //pasamos la devolucion a DOM
+    
 })
 
 .catch(function(err){
