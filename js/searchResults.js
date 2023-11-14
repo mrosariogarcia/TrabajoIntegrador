511eff42891a00f3d5d5f7ce7300de88470fc7a6
feli-qm <fquirogamange@udesa.edu.ar>
2023-11-14 16:38:14 -0300

    Continuacion modificaciones en searchResults (JS, HTML), css e index.html

--- js/searchResults.js
+++ js/searchResults.js
@@ -7,7 +7,7 @@ let buscado = resObj.get("nombre");
 const API_KEY = "5990392eb4d2b299d1f882dd6addfeae"
 
 let busquedaMovies = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${buscado}&page=1&include_adult=false`
-let busquedaTv = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=en-US&query=${buscado}&page=1&include_adult=false`
+let busquedaSeries = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=en-US&query=${buscado}&page=1&include_adult=false`
 
 
 let palabraBuscada = document.querySelector("#palabraBuscada");
@@ -50,7 +50,7 @@ fetch(busquedaMovies)
 
             //creamos una variable 'devolucion' para guardar todas las opciones de peliculas y luego, poder mostrarlas en el html
 
-            devolucion += `<article class = "">
+            devolucion += `<article id="claseArticle" class="contenedorPelis">
                             <img src="${imagen}" class="sectionarticleimg">
                             <a href="detailMovie.html?id=${id}">
                                 <h5>${titulo}</h5>
@@ -62,6 +62,60 @@ fetch(busquedaMovies)
 
         //pasamos la devolucion a DOM
         pelisEncontradas.innerHTML = devolucion
+    })
+
+    .catch(function(e){
+        console.log(e)
+    })
+
+/* Fetch para la busqueda de series */
+fetch(busquedaSeries)
+
+    .then(function(response){
+        return response.json()
+    })
+
+    .then(function(data){
+        let arrayInfo = data.results
+   
+        let devolucion;
+        const seriesEncontradas = document.querySelector(".res_busqueda")
+        for (let i=0; i < arrayInfo.length; i++){
+            console.log(arrayInfo[i])
+            //IMAGEN de serie
+            // Si no tiene imagen: 
+            let foto = ''
+            let imagen;
+            //Si tiene imagen: 
+            if (arrayInfo[i].poster_path != null){
+                let foto  = arrayInfo[i].poster_path
+                imagen = `https://image.tmdb.org/t/p/w342/${foto}`
+            }
+            
+            //titulo de serie
+            
+            let titulo = arrayInfo[i].title
+            
+            //id de serie
+            let id = arrayInfo[i].id
+
+            //release date de serie
+            let releaseDate = arrayInfo[i].release_date
+
+            //creamos una variable 'devolucion' para guardar todas las opciones de series y luego, poder mostrarlas en el html
+
+            devolucion += `<article id="claseArticle" class="contenedorPelis">
+                            <img src="${imagen}" class="sectionarticleimg">
+                            <a href="detailSerie.html?id=${id}">
+                                <h5>${titulo}</h5>
+                            </a>
+                            <h6>${releaseDate}</h6>
+                        </article>`
+
+        }
+
+        //pasamos la devolucion a DOM
+        seriesEncontradas.innerHTML = devolucion
     })
 
     .catch(function(e){
