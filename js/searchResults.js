93440d96afa7f79968c71905d03c6b65cfe5760a
feli-qm <fquirogamange@udesa.edu.ar>
2023-11-14 15:26:18 -0300

    Modificaciones js searchResults

--- js/searchResults.js
+++ js/searchResults.js
@@ -6,8 +6,7 @@ let buscado = resObj.get("nombre");
 
 const API_KEY = "5990392eb4d2b299d1f882dd6addfeae"
 
-let busquedaPeliculas = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${nombre}&page=1&include_adult=false`
-let busquedaSeries = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${nombre}&include_adult=false`
+let busquedaPeliculasSeries = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${nombre}&page=1&include_adult=false`
 
 let respuesta = document.querySelector(".busqueda");
     respuesta.innerText = nombre;
@@ -22,8 +21,6 @@ fetch(busquedaPeliculas)
     })
 
     .then(function(data){
-
-
         let arrayInfo = data.results
         console.log(arrayInfo)
 
@@ -36,9 +33,8 @@ fetch(busquedaPeliculas)
         for (let i=0; i <=5; i++){
             
                 //IMAGEN de pelicula
-
             // Si no tiene imagen: 
-            let foto = './img/imagen_not_found.jpg'
+            let foto = ''
 
             // Si tiene imagen: 
             if (arrayInfo[i].poster_path != null){
@@ -54,13 +50,13 @@ fetch(busquedaPeliculas)
             let id = arrayInfo[i].id
 
             //release date de pelicula
-            let releaseDate = arrayInfo[1].release_date
+            let releaseDate = arrayInfo[i].release_date
 
             //creamos una variable 'devolucion' para guardar todas las opciones de peliculas y luego, poder mostrarlas en el html
 
-            devolucion += `<article>
-                            <img src="${imagen}">
-                            <a href="detailSerie.html?id=${id}">
+            devolucion += `<article class = "res_busqueda">
+                            <img src="${imagen}" class="sectionarticleimg">
+                            <a href=`detailMovie.html?id=${id}`>
                                 <h5>${titulo}</h5>
                             </a>
                             <h6>${releaseDate}</h6>
@@ -69,9 +65,10 @@ fetch(busquedaPeliculas)
         }
 
         //pasamos la devolucion a DOM
-        devolucionP.innerHTML = devolucion
+        lugarDevolucion.innerHTML = devolucion
     })
 
     .catch(function(e){
+        return e;
         console.log(e)
     })
